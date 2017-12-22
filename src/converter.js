f79962c033f2856c511784116288d6525bc36738
Leonardo Mendes Miraglia <leo.mi.me@gmail.com>
2017-12-22 18:49:30 +0000

    Spinner loader added

--- src/converter.js
+++ src/converter.js
@@ -1,11 +1,18 @@
 const request = require('request');
 const chalk = require('chalk');
+const ora = require('ora')
+
+const spinner = ora({
+  text: 'Retrieving data...',
+  color: 'yellow',
+});
 
 function convert(currency = 'USD', amount = 1) {
   const url = `https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=${currency}&amount=${amount}`;
-  
+  spinner.start();
   request(url, (error, response, body) => {
     let apiResponse;
+    spinner.stop();
     try{
       apiResponse = JSON.parse(body);
       console.log(`${chalk.yellow(amount)} BTC to ${chalk.cyan(currency)} = ${chalk.yellow(apiResponse.price)}`);
