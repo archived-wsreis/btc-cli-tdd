77fcde1976d44d9d05745cd89326b3c2bb231e27
Leonardo Mendes Miraglia <leo.mi.me@gmail.com>
2017-12-22 16:59:02 +0000

    added commander to build cli commands and added description

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
+var program = require('commander')
 
-console.log('Hello World');
+const pkg = require('../package.json');
+
+program
+    .version(pkg.version)
+    .description('Convert Bitcoin to any currency defined')
+    .parse(process.argv)
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -2,14 +2,23 @@ const expect =  require('chai').expect;
 
 const exec = require('child_process').exec;
 
+const pkg = require('../package.json');
+
 const btcConverter = './src/main.js';
 
 
 describe('Main CLI', () => {
-    it('should return Hello World', (done)=>{
-        exec(btcConverter, (err, stdout, stderr) => {
+    it('should return version of cli', (done)=>{
+        exec(`${btcConverter} --version`, (err, stdout, stderr) => {
             if(err) throw err;
-            expect(stdout.replace('\n', '')).to.be.equal('Hello World')
+            expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
+            done();
+        })        
+    })
+    it('should return description when btc-cli-tdd --help', (done)=>{
+        exec(`${btcConverter} --help`, (err, stdout, stderr) => {
+            if(err) throw err;
+            expect(stdout.includes('Convert Bitcoin to any currency defined')).to.be.true;
             done();
         })        
     })
